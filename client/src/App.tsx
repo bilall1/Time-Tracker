cc9bda6fc94ad578a92d343a3d71aa134a08d0da
bilall1 <muhammad.bilal0636@gmail.com>
2025-04-24 22:34:05 +0200

    setup client

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+  
+  return (
+    <div>
+      <header>
+        <h1>Mini Time Tracker</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
